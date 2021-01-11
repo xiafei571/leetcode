e8afe92147d52b0ea4b7e88f23aa8db81e7b734e
夏飞 <xiafei571@gmail.com>
2021-01-11 16:40:31 +0900

    排列组合，借用递归，

---
+++ src/algo/expert/GetPermutations.java
@@ -0,0 +1,58 @@
+package algo.expert;
+
+import java.util.*;
+
+public class GetPermutations {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		// O(N! * N * N) of Time, O(N! * N) of Space
+		List<List<Integer>> perms = getPermutations(list);
+
+		for (List<Integer> perm : perms) {
+			for (Integer num : perm) {
+				System.out.print(num);
+			}
+			System.out.println();
+		}
+	}
+
+	public static List<List<Integer>> getPermutations(List<Integer> array) {
+		// Write your code here.
+		if (array.size() == 0)
+			return new ArrayList<List<Integer>>();
+
+		List<List<Integer>> perms = new ArrayList<List<Integer>>();
+		getPermutationsHelper(array, new ArrayList<Integer>(), perms);
+		// P(3,3) = 3*2*1
+		return perms;
+	}
+
+	private static void getPermutationsHelper(List<Integer> array, List<Integer> perm, List<List<Integer>> perms) {
+		if (array.size() == 0) {
+			perms.add(perm);
+		} else {
+			for (int num : array) {
+				List<Integer> newArray = removeNum(num, array);
+				List<Integer> newPerm = new ArrayList<Integer>();
+				newPerm.addAll(perm);
+				newPerm.add(num);
+				getPermutationsHelper(newArray, newPerm, perms);
+			}
+		}
+
+	}
+
+	private static List<Integer> removeNum(int num, List<Integer> array) {
+		List<Integer> newArr = new ArrayList<Integer>();
+		for (int n : array) {
+			if (n != num) {
+				newArr.add(n);
+			}
+		}
+		return newArr;
+	}
+}

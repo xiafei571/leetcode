4b24a9799fe5bfb77e1e39399e41e08455a1d13e
夏飞 <xiafei571@gmail.com>
2021-01-18 18:12:44 +0900

    排列组合，利用已经排列好的累加

---
+++ src/algo/expert/Powerset.java
@@ -0,0 +1,23 @@
+package algo.expert;
+
+import java.util.*;
+
+public class Powerset {
+	public static List<List<Integer>> powerset(List<Integer> array) {
+		// Write your code here.
+		List<Integer> empty = new ArrayList<Integer>();
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
+		list.add(empty);
+		for (int i = 0; i < array.size(); i++) {
+			int currentNum = array.get(i);
+			int len = list.size();
+			for (int j = 0; j < len; j++) {
+				List<Integer> subList = new ArrayList<Integer>(list.get(j));
+				subList.add(currentNum);
+				list.add(subList);
+			}
+		}
+
+		return list;
+	}
+}

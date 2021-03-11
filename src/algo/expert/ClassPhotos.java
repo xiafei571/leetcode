ace01913e819ab144317c085961e47daeee4edb1
xiafei571 <xiafei571@gmail.com>
2021-03-12 01:35:26 +0900

    简单题，需要先排序，然后找到哪一队在后面，依次对比就可以了

---
+++ src/algo/expert/ClassPhotos.java
@@ -0,0 +1,24 @@
+package algo.expert;
+
+import java.util.*;
+
+public class ClassPhotos {
+	public boolean classPhotos(ArrayList<Integer> redShirtHeights, ArrayList<Integer> blueShirtHeights) {
+		// Write your code here.
+		Collections.sort(redShirtHeights, Collections.reverseOrder());
+		Collections.sort(blueShirtHeights, Collections.reverseOrder());
+
+		String backRow = redShirtHeights.get(0) > blueShirtHeights.get(0) ? "red" : "blue";
+
+		for (int i = 0; i < redShirtHeights.size(); i++) {
+			if (backRow.equals("red") && redShirtHeights.get(i) <= blueShirtHeights.get(i)) {
+				return false;
+			} else if (backRow.equals("blue") && redShirtHeights.get(i) >= blueShirtHeights.get(i)) {
+				return false;
+			} else {
+				continue;
+			}
+		}
+		return true;
+	}
+}

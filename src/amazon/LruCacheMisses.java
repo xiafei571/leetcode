f7d3c3a6d68a838cdf8369c60cfd49a7991c5719
夏飞 <xiafei571@gmail.com>
2021-04-19 20:41:46 +0900

    用HashMap和DoublyLinkedList处理LRU的问题，最近使用的就是双向链表的头，容量满了之后淘汰的就是双向链表的尾，取数据的时候从HashMap根据key取可以O(1)，JAVA的话也可以直接继承LinkedHashMap, 重写一下removeEldestEntry的逻辑即可，往LinkedHashMap里put的时候：the previous value associated with key, or null if there was no mapping for key. (A null return can also indicate that the map previously associated null with key.)

---
+++ src/amazon/LruCacheMisses.java
@@ -0,0 +1,55 @@
+package amazon;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class LruCacheMisses {
+
+	public static void main(String[] args) {
+		List<Integer> pages = new ArrayList<Integer>();
+		pages.add(1);
+		pages.add(2);
+		pages.add(1);
+		pages.add(3);
+		pages.add(1);
+		pages.add(1);
+		System.out.println(lruCacheMisses(6, pages, 2));
+	}
+
+	/**
+	 * LeastRecently-Used
+	 * 
+	 * @param num, the number of pages
+	 * @param pages, the pages requested
+	 * @param maxCacheSize, the size of the cache
+	 * @return integer representing the number of cache misses.
+	 */
+	private static int lruCacheMisses(int num, List<Integer> pages, int maxCacheSize) {
+		LRUCache<Integer, Integer> cache = new LRUCache<Integer, Integer>(maxCacheSize);
+		int miss = 0;
+
+		for (Integer page : pages) {
+			if (cache.put(page, 1) == null) {
+				miss++;
+			}
+		}
+
+		return miss;
+	}
+
+	static class LRUCache<K, V> extends LinkedHashMap<Integer, Integer> {
+		private final int maxCacheSize;
+
+		public LRUCache(int maxCacheSize) {
+			super(maxCacheSize, 1);
+			this.maxCacheSize = maxCacheSize;
+		}
+
+		@Override
+		protected boolean removeEldestEntry(java.util.Map.Entry<Integer, Integer> eldest) {
+			return this.size() > this.maxCacheSize;
+		}
+	}
+
+}

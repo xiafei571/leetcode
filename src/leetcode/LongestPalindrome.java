dc98990829a3db0855d83b191e077ebc28a1ec90
xiafei571 <xiafei571@gmail.com>
2021-04-27 02:40:47 +0900

    最长回文子串用动态规划

--- src/leetcode/LongestPalindrome.java
+++ src/leetcode/LongestPalindrome.java
@@ -4,9 +4,48 @@ public class LongestPalindrome {
 	public static void main(String[] args) {
 //		System.out.println(longestPalindrome("cbbd"));
 		System.out.println(longestPalindrome("babad"));
+		System.out.println(longestPalindrome2("babad"));
 	}
+	
+	private static String longestPalindrome2(String s) { // N^2
+        int begin = 0;
+        int longest = 1;
+        if(s.length() == 1){
+            return s;
+        }
 
-	public static String longestPalindrome(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+
+        for(int i = 0; i < s.length(); i++){
+            dp[i][i] = 1;
+        }
+        //j-i >= 3 : dp[i][j] = dp[i+1][j-1] && s[i] == s[j]
+        //j-i < 3 : s[i] == s[j]
+        for(int j = 1; j < s.length(); j++){
+            for(int i = 0; i < j + 1; i++){
+                if(s.charAt(i) == s.charAt(j)){
+                    if(j - i < 3){
+                        dp[i][j] = 1;
+                    }else{
+                        if(dp[i+1][j-1] == 1){
+                            dp[i][j] = 1;
+                        }
+                    }
+                }else{
+                    continue;
+                }
+
+                if(dp[i][j] == 1 && j - i + 1 > longest){
+                    begin = i;
+                    longest = j - i + 1;
+                }
+            }
+        }
+
+        return s.substring(begin, begin+longest);
+    }
+
+	public static String longestPalindrome(String s) {//N^3
 		if (isPalindrome(s)) {
 			return s;
 		}
@@ -25,17 +64,19 @@ public class LongestPalindrome {
 	}
 
 	private static boolean isPalindrome(String s) {
-		int i = 0; 
-//		for (int i = 0; i < s.length() / 2; i++) {
-//			for (int j = s.length()-1; j >= s.length() / 2; j--) {
-////				if(i == j)
-////					return true;
-//				
-//				if (s.charAt(i) != s.charAt(j))
-//					return false;
-//
-//			}
-//		}
+		if(s.length() == 1) {
+			return true;
+		}
+		
+		int left = 0;
+		int right = s.length() - 1;
+		while(left < right) {
+			if(s.charAt(left) != s.charAt(right)) {
+				return false;
+			}
+			right--;
+			left++;
+		}
 		return true;
 	}
 }

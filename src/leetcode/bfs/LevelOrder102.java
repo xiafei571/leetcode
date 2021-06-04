b532554e5a18eaab24e5fd86fb21e96df1529faa
xiafei571 <xiafei571@gmail.com>
2021-06-04 15:06:42 +0900

    BFS，注意每层一个数组，需要获取一下queue的size来给每层添加一个数组。103在102的基础上需要加一个Deque数组，保证结果的插入顺序

---
+++ src/leetcode/bfs/LevelOrder102.java
@@ -0,0 +1,73 @@
+package leetcode.bfs;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+public class LevelOrder102 {
+	/*
+	 * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+	 * 
+	 *  
+	 * 
+	 * 示例： 二叉树：[3,9,20,null,null,15,7],
+	 */
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		if(root == null){
+            return result;
+        }
+		
+		Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+		queue.add(root);
+		while(!queue.isEmpty()) {
+			int n = queue.size();
+			List<Integer> currLayer = new ArrayList<Integer>();
+			while(n >=0) {
+				TreeNode node = queue.poll();
+				currLayer.add(node.val);
+				
+				if(node.left != null) {
+					queue.add(node.left);
+				}
+				
+				if(node.right != null) {
+					queue.add(node.right);
+				}
+				n--;
+			}
+			
+			result.add(currLayer);
+			
+		}
+		
+		return result;
+    }
+	
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}
---
+++ src/leetcode/bfs/ZigzagLevelOrder103.java
@@ -0,0 +1,75 @@
+package leetcode.bfs;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ZigzagLevelOrder103 {
+	/*
+	 * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+	 * 
+	 * 例如： 给定二叉树 [3,9,20,null,null,15,7],
+	 */
+	public List<List<Integer>> zigzagLevelOrder(TreeNode root){
+		List<List<Integer>> result = new LinkedList<List<Integer>>();
+		
+		if(root == null) {
+			return result;
+		}
+		
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.add(root);
+		boolean orderLeft = true;
+		// 0:left->right, 1:right -> left
+		
+		while(!queue.isEmpty()) {
+			int n = queue.size();
+			Deque<Integer> deque = new LinkedList<Integer>();
+			while(n > 0) {
+				TreeNode node = queue.poll();
+				if(orderLeft) {
+					deque.addLast(node.val);
+				}else {
+					deque.addFirst(node.val);
+				}
+				
+				if(node.left != null) {
+					queue.add(node.left);
+				}
+				
+				if(node.right != null) {
+					queue.add(node.right);
+				}
+				
+				n--;
+			}
+			
+			orderLeft = !orderLeft;
+			result.add(new LinkedList<Integer>(deque));
+		}
+		
+		return result;
+		
+	}
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+}

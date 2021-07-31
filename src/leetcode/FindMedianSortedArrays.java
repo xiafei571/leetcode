652646ffdfc6f5e153a86e7fdd1056700cf2a258
xiafei571 <xiafei571@gmail.com>
2021-07-31 18:58:15 +0900

    两种方案，第二种O(log(m+n)) 需要用二分

--- src/leetcode/FindMedianSortedArrays.java
+++ src/leetcode/FindMedianSortedArrays.java
@@ -1,7 +1,7 @@
 package leetcode;
 
 public class FindMedianSortedArrays {
-	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
 		// odd 1 3 5 7 ...
 		// even 2 4 6 8 ...
 		int[] nums3 = new int[nums1.length + nums2.length];
@@ -28,8 +28,46 @@ public class FindMedianSortedArrays {
 				}
 			}
 		}
-
 		return 0;
+	}
 
+	public static void main(String[] args) {
+		System.out.println(findMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 }));
+		System.out.println(findMedianSortedArrays2(new int[] { 1, 2 }, new int[] { 3, 4 }));
+	}
+
+	public static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+		if ((nums1.length + nums2.length) % 2 == 0) {
+			return 0.5 * (findKth(nums1, 0, nums2, 0, (nums1.length + nums2.length + 1) / 2)
+					+ findKth(nums1, 0, nums2, 0, (nums1.length + nums2.length + 2) / 2));
+		} else {
+			return findKth(nums1, 0, nums2, 0, (nums1.length + nums2.length + 2) / 2);
+		}
+	}
+
+	private static double findKth(int[] nums1, int start1, int[] nums2, int start2, int k) {
+		int len1 = nums1.length - start1;
+		int len2 = nums2.length - start2;
+
+		if (len1 > len2) {
+			return findKth(nums2, start2, nums1, start1, k);
+		}
+
+		if (len1 == 0) {
+			return nums2[start2 + k - 1];
+		}
+
+		if (k == 1) {
+			return Math.min(nums1[start1], nums2[start2]);
+		}
+
+		int i = start1 + Math.min(len1, k / 2) - 1;
+		int j = start2 + Math.min(len2, k / 2) - 1;
+
+		if (nums1[i] < nums2[j]) {
+			return findKth(nums1, i + 1, nums2, start2, k - Math.min(len1, k / 2));
+		} else {
+			return findKth(nums1, start1, nums2, j + 1, k - Math.min(len2, k / 2));
+		}
 	}
 }

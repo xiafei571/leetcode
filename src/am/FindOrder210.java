3377ea4349cc8fced27f74fe2844c264b9d7e86d
xiafei571 <xiafei571@gmail.com>
2021-08-04 23:00:15 +0900

    拓扑排序

---
+++ src/am/FindOrder210.java
@@ -0,0 +1,63 @@
+package am;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+public class FindOrder210 {
+	// numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+	//[0,2,1,3]
+	
+	public static void main(String[] args) {
+		
+		int[] array = findOrder(2, new int[][] {{1,0}});
+		System.out.println(array.length);
+	}
+	
+	public static int[] findOrder(int numCourses, int[][] prerequisites) {
+		int[] indegree = new int[numCourses];
+		Map<Integer, Set<Integer>> graph = new HashMap<Integer, Set<Integer>>();
+		
+		for(int[] pre : prerequisites) {
+			indegree[pre[0]]++;
+			Set<Integer> childs = graph.getOrDefault(pre[1], new HashSet<Integer>());
+			childs.add(pre[0]);
+			graph.put(pre[1], childs);
+		}
+		
+		Queue<Integer> queue = new LinkedList<Integer>();
+		
+		for(int i = 0; i < indegree.length; i++) {
+			if(indegree[i] == 0) {
+				queue.add(i);
+			}
+		}
+		
+		int[] result = new int[numCourses];
+		int idx = 0;
+		while(queue.size() > 0) {
+			int top = queue.poll();
+			result[idx] = top;
+			idx++;
+			
+			Set<Integer> childs = graph.get(top);
+			if(childs != null && childs.size() > 0) {
+				for(int child : childs) {
+					indegree[child]--;
+					if(indegree[child] == 0) {
+						queue.add(child);
+					}
+				}
+			}
+		}
+		
+		if(idx == numCourses) {
+			return result;
+		}
+		
+		return new int[] {};
+	}
+}

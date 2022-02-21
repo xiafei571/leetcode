e1fdc96c43aad77e608ac1a111aa7f887c441426
xiafei571 <xiafei571@gmail.com>
2022-02-22 00:51:33 +0900

    case desc

--- src/am/IsValidBST98.java
+++ src/am/IsValidBST98.java
@@ -20,6 +20,13 @@ public class IsValidBST98 {
 		}
 	}
 
+	/*
+	 * case:
+	 *        15
+	 * 4           26 [15:null]
+	 *     18[15:26]   27[26:null]
+	 * 14[15:18] 20[18:26]           
+	 */
 	public boolean isValidBST(TreeNode root) {
 		return isValidBST(root, null, null);
 	}

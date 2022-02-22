cb7e6b88649d4daefd15dd0a1a0f5106205d0863
夏飞 <xiafei571@gmail.com>
2022-02-22 16:49:52 +0900

    考虑负值 损失最小

--- src/am/MaxProfit121.java
+++ src/am/MaxProfit121.java
@@ -1,20 +1,24 @@
 package am;
 
 public class MaxProfit121 {
-	public int maxProfit(int[] prices) {
-		int max = 0;
+	public static int maxProfit(int[] prices) {
+		int max = Integer.MIN_VALUE;
 		int j = 1;
 		int min = prices[0];
 
 		while (j < prices.length) {
-			if (prices[j] > min) {
-				max = Math.max(max, prices[j] - min);
-			}
+			//if (prices[j] > min) {
+			max = Math.max(max, prices[j] - min);
+			//}
 
 			min = Math.min(min, prices[j]);
 			j++;
 		}
 
 		return max;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(maxProfit(new int[] {7,6,5,4,3,1}));
 	}
 }

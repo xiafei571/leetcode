8bf8738c192db3ac316c5dfb7417d93e6b6b8b00
夏飞 <xiafei571@gmail.com>
2022-05-27 00:00:56 +0900

    经典LCA算法

---
+++ src/bytedance/LowestCommonAncestor235.java
@@ -0,0 +1,15 @@
+package bytedance;
+
+import common.TreeNode;
+
+public class LowestCommonAncestor235 {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root.val > p.val && root.val > q.val) {
+			return lowestCommonAncestor(root.left, p, q);
+		} else if (root.val < p.val && root.val < q.val) {
+			return lowestCommonAncestor(root.right, p, q);
+		} else {
+			return root;
+		}
+	}
+}

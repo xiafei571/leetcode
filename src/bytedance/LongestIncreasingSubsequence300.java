4c95d8e2abf3900dfc9ff758fb49ae5022bf85a1
夏飞 <xiafei571@gmail.com>
2022-05-27 15:21:14 +0900

    dp n^2 解

--- src/bytedance/LongestIncreasingSubsequence300.java
+++ src/bytedance/LongestIncreasingSubsequence300.java
@@ -1,5 +1,22 @@
 package bytedance;
 
-public class LongestIncreasingSubsequence300 {
+import java.util.Arrays;
 
+public class LongestIncreasingSubsequence300 {
+	public int lengthOfLIS(int[] nums) {
+		int[] dp = new int[nums.length];
+		Arrays.fill(dp, 1);
+		int res = 1;
+
+		for (int i = 1; i < nums.length; i++) {
+			for (int j = 0; j < i; j++) {
+				if (nums[i] > nums[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+					res = Math.max(res, dp[i]);
+				}
+			}
+		}
+
+		return res;
+	}
 }

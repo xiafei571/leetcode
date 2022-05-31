4fa3d23fd9e30645ad6ef75f52ff4faa62da994b
夏飞 <xiafei571@gmail.com>
2022-05-31 15:36:14 +0900

    先分组 再旋转

--- src/bytedance/ReverseKGroup25.java
+++ src/bytedance/ReverseKGroup25.java
@@ -1,5 +1,62 @@
 package bytedance;
 
-public class ReverseKGroup25 {
+import java.util.ArrayList;
+import java.util.List;
 
+import common.ListNode;
+
+public class ReverseKGroup25 {
+	public ListNode reverseKGroup(ListNode head, int k) {
+		List<ListNode> heads = new ArrayList<>();
+		List<Integer> group = new ArrayList<>();
+
+		ListNode p = head;
+		while (p != null) {
+			heads.add(p);
+			int cnt = 1;
+			while (cnt < k && p.next != null) {
+				p = p.next;
+				cnt++;
+			}
+			group.add(cnt);
+			ListNode temp = p.next;
+			p.next = null;
+			p = temp;
+		}
+
+		// reverse
+		for (int i = 0; i < heads.size(); i++) {
+			ListNode h = heads.get(i);
+			if (group.get(i) == k) {
+				h = reverse(h);
+			}
+			heads.set(i, h);
+		}
+
+		// combin
+		for (int i = 0; i < heads.size() - 1; i++) {
+			ListNode h = heads.get(i);
+			while (h.next != null) {
+				h = h.next;
+			}
+			h.next = heads.get(i + 1);
+		}
+
+		return heads.get(0);
+	}
+
+	private ListNode reverse(ListNode head) {
+		ListNode last = null;
+		ListNode curr = head;
+		ListNode next;
+
+		while (curr != null) {
+			next = curr.next;
+			curr.next = last;
+			last = curr;
+			curr = next;
+		}
+
+		return last;
+	}
 }

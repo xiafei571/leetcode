94fd9950d90c51888d81c7c892e1580da50328cb
夏飞 <xiafei571@gmail.com>
2022-05-31 13:32:17 +0900

    easy 但是也要小心

--- src/bytedance/CountBinarySubstrings696.java
+++ src/bytedance/CountBinarySubstrings696.java
@@ -1,5 +1,24 @@
 package bytedance;
 
 public class CountBinarySubstrings696 {
+	public int countBinarySubstrings(String s) {
+		// if(s == null || s.length() <= 1){
+		// return 0;
+		// }
+		int curr = 1;
+		int pre = 0;
+		int count = 0;
 
+		for (int i = 1; i < s.length(); i++) {
+			if (s.charAt(i) == s.charAt(i - 1)) {
+				curr++;
+			} else {
+				count += Math.min(pre, curr);
+				pre = curr;
+				curr = 1;
+			}
+		}
+
+		return count += Math.min(pre, curr);
+	}
 }

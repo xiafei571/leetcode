77e17812a86209140fd8044a8aa28f2f1bd43c53
夏飞 <xiafei571@gmail.com>
2022-06-02 15:15:51 +0900

    一旦找到不同的，就直接返回结果，如果一直都相同，那么需要判断一下长度

--- src/bytedance/IsAlienSorted953.java
+++ src/bytedance/IsAlienSorted953.java
@@ -1,5 +1,54 @@
 package bytedance;
 
 public class IsAlienSorted953 {
+	public boolean isAlienSorted(String[] words, String order) {
 
+		for (int i = 0; i < order.length(); i++) {
+			char ch = order.charAt(i);
+			setSeq(ch, i);
+		}
+
+		for (int i = 0; i < words.length - 1; i++) {
+			String a = words[i];
+			String b = words[i + 1];
+			if (!compare(a, b)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private static int charSeq[] = new int[26];
+
+	private int getSeq(char ch) {
+		return charSeq[ch - 'a'];
+	}
+
+	private void setSeq(char ch, int seq) {
+		charSeq[ch - 'a'] = seq;
+	}
+
+	private boolean compare(String a, String b) {
+
+		for (int i = 0; i < Math.min(a.length(), b.length()); i++) {
+			char c1 = a.charAt(i);
+			char c2 = b.charAt(i);
+			// the first different letter
+			if (c1 != c2) {
+				// if(getSeq(c1) > getSeq(c2)){
+				// return false;
+				// }else{ // case: ["kuvp","q"] "ngxlkthsjuoqcpavbfdermiywz"
+				// return true;
+				// }
+				return getSeq(c1) <= getSeq(c2);
+			}
+		}
+
+		if (a.length() > b.length()) {// case: ["apple","app"] "abcdefghijklmnopqrstuvwxyz"
+			return false;
+		}
+
+		return true;
+	}
 }

315a285c4ed0d684dec2f1aaf283930d94301530
夏飞 <xiafei571@gmail.com>
2022-06-03 13:55:48 +0900

    如果需要打印结果，那么就不用dp，用backtrack

--- src/bytedance/LongestIncreasingSubsequence300.java
+++ src/bytedance/LongestIncreasingSubsequence300.java
@@ -1,9 +1,11 @@
 package bytedance;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class LongestIncreasingSubsequence300 {
-	public int lengthOfLIS(int[] nums) {
+	private static int lengthOfLIS(int[] nums) {
 		int[] dp = new int[nums.length];
 		Arrays.fill(dp, 1);
 		int res = 1;
@@ -19,4 +21,42 @@ public class LongestIncreasingSubsequence300 {
 
 		return res;
 	}
+	
+	public static void main(String[] args) {
+		int[] input = {10,9,2,5,3,7,101,18};
+		System.out.println(lengthOfLIS(input));
+		System.out.println(lengthOfLIS2(input));
+	}
+	
+	private static int lengthOfLIS2(int[] nums) {
+		List<Integer> res = new ArrayList<>();
+		
+		for(int i = 0; i < nums.length-1; i++) {
+			List<Integer> curr = new ArrayList<>();
+			curr.add(nums[i]);
+			backtrack(nums, curr, i+1, res);
+			curr.remove(curr.size()-1);
+		}
+		
+		for(Integer num : res) {
+			System.out.println(num);
+		}
+		
+		return res.size();
+	}
+	
+	private static void backtrack(int[] nums, List<Integer> curr, int idx, List<Integer> res) {
+		for(int j = idx; j < nums.length; j++) {
+			if(nums[j] > curr.get(curr.size() - 1)) {
+				curr.add(nums[j]);
+				if(curr.size() > res.size()) {
+					res.clear();
+					res.addAll(curr);
+				}
+				backtrack(nums, curr, j+1, res);
+				curr.remove(curr.size()-1);
+			}
+		}
+	}
+	
 }

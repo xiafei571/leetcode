cb2ba3008d10b326ab605913e9a57871a74250cb
夏飞 <xiafei571@gmail.com>
2022-06-06 11:41:24 +0900

    dp[i] = dp[j] && set.contains(j-i);

--- src/bytedance/WordBreak139.java
+++ src/bytedance/WordBreak139.java
@@ -1,5 +1,26 @@
 package bytedance;
 
-public class WordBreak139 {
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class WordBreak139 {
+	public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        
+        for(int i = 1; i < s.length() + 1; i++){
+            for(int j = 0; j < i; j++){
+                if(dp[j] && set.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[s.length()];
+        
+    }
 }

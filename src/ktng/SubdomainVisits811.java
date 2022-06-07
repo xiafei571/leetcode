de5b25a9c8e9a4264e9a47ab93151e63a2cc0a7b
夏飞 <xiafei571@gmail.com>
2022-06-07 16:58:39 +0900

    easy

---
+++ src/ktng/SubdomainVisits811.java
@@ -0,0 +1,38 @@
+package ktng;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SubdomainVisits811 {
+	public List<String> subdomainVisits(String[] cpdomains) {
+		List<String> res = new ArrayList<>();
+		Map<String, Integer> subdomainVisitsMap = new HashMap<>();
+
+		for (String cpdomain : cpdomains) {
+			String[] temp = cpdomain.split(" ");
+			Integer visit = Integer.valueOf(temp[0]);
+			String domain = temp[1];
+
+			String[] subdomains = domain.split("\\.");
+			String subdomain = null;
+			for (int i = subdomains.length - 1; i >= 0; i--) {
+				if (subdomain == null) {
+					subdomain = subdomains[i];
+				} else {
+					subdomain = subdomains[i] + "." + subdomain;
+				}
+
+				Integer curr = subdomainVisitsMap.getOrDefault(subdomain.toString(), 0);
+				subdomainVisitsMap.put(subdomain.toString(), curr + visit);
+			}
+		}
+
+		for (String subdomain : subdomainVisitsMap.keySet()) {
+			res.add(subdomainVisitsMap.get(subdomain) + " " + subdomain);
+		}
+
+		return res;
+	}
+}
